5e7ff80aaff1a6aebd2d57f23f1e92b94fe53cdf
Maks <avto12@i.ua>
2016-01-26 01:50:31 +0200

    Fat Jar Test_task.jar created

--- src/main/java/org/App.java
+++ src/main/java/org/App.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
  * Created by maks(avto12@i.ua) on 25.01.2016.
  */
 public class App {
-    private static final Logger logger = LoggerFactory.getLogger(App.class);
+    static final Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main( String[] args ) {
         Service service=ServiceImpl.getInstance();
